5a46957d75e2acfac52c667ee4248fadd5fbe332
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-08-10 15:33:50 -0400

    updates

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  //useUnifiedTopology: true
+  useUnifiedTopology: true
 });
 
 app.use(require("./routes/api.js"));
